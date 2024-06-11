deb295cb1e48fba0972ab7ca195273a89aa0ab65
Hunter Koppen <hunter.koppen@kobeon.nl>
2024-06-11 19:36:18 +0200

    removed debugger

--- src/components/InitializeGTM.jsx
+++ src/components/InitializeGTM.jsx
@@ -1,10 +1,11 @@
 import TagManager from "react-gtm-module";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 
 export function InitializeGTM(props) {
+    const initializedRef = useRef(false);
+
     useEffect(() => {
-        if (!mx.ga4Connected) {
-            // flag to prevent onNavigation from sending multiple page views after things like after widget load, after show page, etc.
+        const initializeGTM = () => {
             if (props.gtmId.status !== "available") {
                 // if the data is not yet available then we don't do anything
                 return;
@@ -17,107 +18,109 @@ export function InitializeGTM(props) {
                 }
             }
 
-            const origOnNavigation = mx.ui.getContentForm().onNavigation; // save what the original onNavigation function did
-            mx.ui.getContentForm().onNavigation = () => {
-                // Custom Actions
-                const existingGTM = document.querySelector(`script[src*="${props.gtmId.value}"]`);
-                
-                if (!existingGTM) {
-                    if (props.sendCustomProps === false) {
-                        const tagManagerArgsInitialize = {
-                            gtmId: props.gtmId.value
-                        };
+            // Initialize GTM script only once
+            if (!initializedRef.current) {
+                const tagManagerArgsInitialize = {
+                    gtmId: props.gtmId.value
+                };
+                TagManager.initialize(tagManagerArgsInitialize);
+                initializedRef.current = true;
+            }
 
-                        TagManager.initialize(tagManagerArgsInitialize);
+            const dataLayerStructure = () => {
+                let dataLayer = '{"event":"' + props.pageViewEventName + '",'; // initialize the dataLayer variable
+
+                if (props.sendPageTitle) {
+                    // send page title
+                    dataLayer += '"Page Name":"' + mx.ui.getContentForm().title + '",';
+                }
+
+                if (props.sendModuleLocation) {
+                    // send module location
+                    const modulePath = mx.ui.getContentForm().path;
+                    // eslint-disable-next-line
+                    const moduleLocation = function (modPath) {
+                        const pageExtension = ".page.xml";
+                        const path = modPath.substr(0, modPath.length - pageExtension.length);
+                        return path;
+                    };
+                    dataLayer += '"Module Location":"' + moduleLocation(modulePath) + '",';
+                }
+
+                if (props.sendPageURL) {
+                    // send page URL
+                    let pageURL;
+                    if (mx.ui.getContentForm().url !== null) {
+                        pageURL = window.location.origin + mx.ui.getContentForm().url;
                     } else {
-                        const dataLayerStructure = () => {
-                            var dataLayer = '{"event":"' + props.pageViewEventName + '",'; // initialize the dataLayer variable
+                        pageURL = window.location.origin;
+                    }
 
-                            if (props.sendPageTitle) {
-                                // send page title
-                                dataLayer += '"Page Name":"' + mx.ui.getContentForm().title + '",';
-                            }
+                    // eslint-disable-next-line
+                    const trimmedURL = function (fullURL) {
+                        const lastCharIndex = fullURL.lastIndexOf("/");
+                        const endString = fullURL.substring(lastCharIndex + 1, fullURL.length);
+                        if (isNaN(endString)) {
+                            return fullURL; // the end of the string isn't a number, return the whole thing
+                        } else {
+                            return fullURL.substr(0, lastCharIndex); // the end of the string is a number, trim it
+                        }
+                    };
+                    dataLayer += '"Page URL":"' + trimmedURL(pageURL) + '",';
+                }
 
-                            if (props.sendModuleLocation) {
-                                // send module location
-                                var modulePath = mx.ui.getContentForm().path;
-                                // eslint-disable-next-line
-                                var moduleLocation = function (modPath) {
-                                    var pageExtension = ".page.xml";
-                                    var path = modPath.substr(0, modPath.length - pageExtension.length);
-                                    return path;
-                                };
-                                dataLayer += '"Module Location":"' + moduleLocation(modulePath) + '",';
-                            }
+                if (props.sendSessionID) {
+                    // send session ID
+                    dataLayer += '"Session ID":"' + mx.session.getSessionObjectId() + '",';
+                }
 
-                            if (props.sendPageURL) {
-                                // send page URL
-                                var pageURL;
-                                if (mx.ui.getContentForm().url !== null) {
-                                    pageURL = window.location.origin + mx.ui.getContentForm().url;
-                                } else {
-                                    pageURL = window.location.origin;
-                                }
+                if (props.sendAdditionalProps) {
+                    // send additional properties
+                    let expressionResult = "";
 
-                                // eslint-disable-next-line
-                                var trimmedURL = function (fullURL) {
-                                    var lastCharIndex = fullURL.lastIndexOf("/");
-                                    var endString = fullURL.substring(lastCharIndex + 1, fullURL.length);
-                                    if (isNaN(endString)) {
-                                        return fullURL; // the end of the string isn't a number, return the whole thing
-                                    } else {
-                                        return fullURL.substr(0, lastCharIndex); // the end of the string is a number, trim it
-                                    }
-                                };
-                                dataLayer += '"Page URL":"' + trimmedURL(pageURL) + '",';
-                            }
+                    for (const line of props.additionalProps) {
+                        for (const object of line.propDataSource.items) {
+                            // object is an item in the list that is returned from the data source
+                            expressionResult += line.propValue.get(object).value + ", ";
+                        }
 
-                            if (props.sendSessionID) {
-                                // send session ID
-                                dataLayer += '"Session ID":"' + mx.session.getSessionObjectId() + '",';
-                            }
-
-                            if (props.sendAdditionalProps) {
-                                // send additional properties
-                                let expressionResult = "";
-
-                                for (const line of props.additionalProps) {
-                                    for (const object of line.propDataSource.items) {
-                                        // object is an item in the list that is returned from the data source
-                                        expressionResult += line.propValue.get(object).value + ", ";
-                                    }
-
-                                    expressionResult = expressionResult.replace(/,\s*$/, "");
-                                    dataLayer += '"' + line.propName + '":"' + expressionResult + '",';
-                                    expressionResult = "";
-                                }
-                            }
-
-                            dataLayer = dataLayer.replace(/,\s*$/, ""); // remove the last comma from the dataLayer variable
-                            dataLayer += "}";
-                            return JSON.parse(dataLayer);
-                        };
-
-                        const dataLayer = dataLayerStructure(props);
-
-                        const tagManagerArgs = {
-                            gtmId: props.gtmId.value,
-                            dataLayer
-                        };
-
-                        TagManager.initialize(tagManagerArgs);
+                        expressionResult = expressionResult.replace(/,\s*$/, "");
+                        dataLayer += '"' + line.propName + '":"' + expressionResult + '",';
+                        expressionResult = "";
                     }
                 }
 
-                mx.ga4Connected = true; // We have sent the page hit, now toggle the switch off to avoid sending duplicates
-
-                // Original Actions
-                origOnNavigation();
-
-                return null;
+                dataLayer = dataLayer.replace(/,\s*$/, ""); // remove the last comma from the dataLayer variable
+                dataLayer += "}";
+                return JSON.parse(dataLayer);
             };
-        }
-    }); // no dependency array -> trigger on every load
+
+            const dataLayer = dataLayerStructure(props);
+
+            // Update the data layer with the latest information
+            TagManager.dataLayer({ dataLayer });
+
+            mx.ga4Connected = true; // We have sent the page hit, now toggle the switch off to avoid sending duplicates
+        };
+
+        const origOnNavigation = mx.ui.getContentForm().onNavigation; // save what the original onNavigation function did
+        mx.ui.getContentForm().onNavigation = () => {
+            initializeGTM();
+            // Original Actions
+            origOnNavigation();
+        };
+
+        initializeGTM(); // Initialize once on load
+    }, [
+        props.gtmId,
+        props.additionalProps,
+        props.pageViewEventName,
+        props.sendPageTitle,
+        props.sendModuleLocation,
+        props.sendPageURL,
+        props.sendSessionID,
+        props.sendAdditionalProps
+    ]); // dependency array to avoid unnecessary loops
 
     return null;
 }
